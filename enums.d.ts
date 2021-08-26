f7893bf72150a9e3e09b82a125655e576c965c5f
Thomas Proffen <thomasproffen@gmail.com>
2021-08-26 09:41:00 -0400

    Cleanup more

--- enums.d.ts
+++ enums.d.ts
@@ -23,33 +23,4 @@
     P20 = 30,
     }
 
-
-    declare const enum RemoteButton {
-    Power = 0x00,
-    VolUp = 0x01,
-    FuncStop = 0x02,
-    LeftTwo = 0x04,
-    Suspended = 0x05,
-    RightTwo = 0x06,
-    Down = 0x08,
-    VolDown = 0x09,
-    Up = 0x0a,
-    Zero = 0x0c,
-    EQ = 0x0d,
-    StRept = 0x0e,
-    One = 0x10,
-    Two = 0x11,
-    Three = 0x12,
-    Four = 0x14,
-    Five = 0x15,
-    Six = 0x16,
-    Seven = 0x18,
-    Eight = 0x19,
-    Nine = 0x1a,
-    }
-declare namespace maqueenIR {
-}
-declare namespace maqueenIRV2 {
-}
-
 // Auto-generated. Do not edit. Really.
